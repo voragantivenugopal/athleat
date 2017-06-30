b012518a38ab76dd27f4034742ed2fbf8d1e5f82
Venugopal Voraganti <venugopal@anipr.in>
2017-06-30 10:30:30 +0530

    done

--- athleatapp/views.py
+++ athleatapp/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from athleat.settings import *
+import xmlrpclib
 # Create your views here.
 
 def doLogin(request):
@@ -16,4 +18,9 @@ def Index(request):
 def mealBuilder(request):
 
 	return render(request,'meal-builder.html',{})
+
+def getUserId(request):
+
+	sock_common = xmlrpclib.ServerProxy(str(XMLRPC_URL) + 'xmlrpc/common')
+	uid = sock_common.login(DB_NAME, USERNAME, PASSWORD)
 
