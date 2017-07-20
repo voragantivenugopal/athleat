ad842d80df817b8e720740e2c2aace8c6525f739
Prashanth <prashanth@anipr.local>
2017-07-20 14:51:26 +0530

    'MealPlanPage'

--- static/wp-content/plugins/foodery-fit-meal-buider/public/js/foodery-fit-meal-builder-publicef15.js
+++ static/wp-content/plugins/foodery-fit-meal-buider/public/js/foodery-fit-meal-builder-publicef15.js
@@ -1,13 +1,14 @@
 
 (function( $ ) {
 
-
+// Start Button 
 $(document).on('click','#mStartBtn',function(e){
     console.log('start here button');
     $('#tab1').removeClass('active');
     $('#tab2').addClass('active');
+    $('[data-target="#step1"]').removeClass('active');
+    $('[data-target="#step2"]').addClass('active');
 });
-
 
 
 
