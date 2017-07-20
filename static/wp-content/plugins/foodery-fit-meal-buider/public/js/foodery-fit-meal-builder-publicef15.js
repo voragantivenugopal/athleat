7f7bf05d5c3fe30386a8338430f4286d849ec60c
fayaz <fayaz@anipr.in>
2017-07-20 14:50:09 +0530

    Adding active and remove functionality for panel buttons

--- static/wp-content/plugins/foodery-fit-meal-buider/public/js/foodery-fit-meal-builder-publicef15.js
+++ static/wp-content/plugins/foodery-fit-meal-buider/public/js/foodery-fit-meal-builder-publicef15.js
@@ -7,9 +7,26 @@ $(document).on('click','#mStartBtn',function(e){
     $('#tab1').removeClass('active');
     $('#tab2').addClass('active');
 });
-
-
-
+$(document).on('click','#mChooseMealPlanBtn',function(e){
+    console.log('start here button');
+    $('#tab2').removeClass('active');
+    $('#tab3').addClass('active');
+});
+$(document).on('click','#mGenderBtn',function(e){
+    console.log('start here button');
+    $('#tab3').removeClass('active');
+    $('#tab4').addClass('active');
+});
+$(document).on('click','#mHowManyWeeksBtn',function(e){
+    console.log('start here button');
+    $('#tab4').removeClass('active');
+    $('#tab5').addClass('active');
+});
+$(document).on('click','#mMealsPerDayBtn',function(e){
+    console.log('start here button');
+    $('#tab5').removeClass('active');
+    $('#tab6').addClass('active');
+});
 
 
 })( jQuery );
