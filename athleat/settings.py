36c370f49c83e0c2459ea33c3e052206d6171ec8
fayaz <fayaz@anipr.in>
2017-07-26 16:56:23 +0530

    Adding athleat tab code

--- athleat/settings.py
+++ athleat/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'athleatmeal',
-        'USER': 'postgres',
+        'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '',
