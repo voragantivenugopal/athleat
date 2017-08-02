93e0ce2e7ea05635c3baf405f8dfed772e6aa51e
fayaz <fayaz@anipr.in>
2017-08-02 13:24:18 +0530

    fetching latest code

--- static/wp-content/plugins/foodery-fit-meal-buider/public/js/foodery-fit-meal-builder-publicef15.js
+++ static/wp-content/plugins/foodery-fit-meal-buider/public/js/foodery-fit-meal-builder-publicef15.js
@@ -1133,10 +1133,10 @@ $(document).on('click','#mSubmitBtn',function(){
     // location.reload();
     });
 function clearPriceCookies(){
-    Cookies.remove('mTotalPrice');
-    Cookies.remove('mTotalFat');
-    Cookies.remove('mTotalProtein');
-    Cookies.remove('mTotalCarb');
+    Cookies.remove('mPriceTotal');
+    Cookies.remove('mFatTotal');
+    Cookies.remove('mProteinTotal');
+    Cookies.remove('mCarbTotal');
 }
 function clearCookies(){
     clearPriceCookies();
